8e00432eed6330156853668aa484b2a0abe4e49f
ammarun11 <cintadamai46@gmail.com>
2019-10-08 11:42:48 +0700

    add axis to signal diskrit uhuy

--- Pertemuan1/diskrit_palo.m
+++ Pertemuan1/diskrit_palo.m
@@ -5,3 +5,7 @@ stem (x,xn);                    % memplot sinyal xn pada batas n
 xlabel('Waktu Diskrit 17101109(t)');
 ylabel('Nilai x(n)');
 title('Sinyal Diskrit 17101109');
+
+% HASIL DATA : 1. Sesuai Modul 2.Menggunakan NIM
+% xn = [1 7 1 0 1 1 0 9];
+% n = [-2 -1 0 1 2 3 4 5]; 
